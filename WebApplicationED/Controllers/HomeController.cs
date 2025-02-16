6245089c79616bd7a7ebfee8285f663402778816
qLepeshka <qlepeshka@gmail.com>
2025-02-16 12:28:47 +0300

    add Education application first commit

---
+++ WebApplicationED/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationED.Models;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace WebApplicationED.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly IConfiguration _config;
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        {
+            _logger = logger;
+
+            _config = config;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public IActionResult Index()
+        {
+            var items = GetAllUsers();
+
+            return View(items);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private List<User> GetAllUsers()
+        {
+            using (IDbConnection db = Connection)
+            {
+                List<User> result = db.Query<User>("SELECT * FROM Users ").ToList();
+
+                return result;
+            }
+        }
+
+        public class User
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public double Balance { get; set; }
+
+            public DateTime Created { get; set; }
+
+        }
+
+
+    }
+}
